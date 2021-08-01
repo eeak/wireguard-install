a683393fd56266e43907d5632a35407169b66c56
eeak <39026644+eeak@users.noreply.github.com>
2021-08-01 23:19:23 +0300

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -88,7 +88,7 @@ function installQuestions() {
 	done
 
 	until [[ ${SERVER_WG_IPV4} =~ ^([0-9]{1,3}\.){3} ]]; do
-		read -rp "Server's WireGuard IPv4: " -e -i 10.66.66.1 SERVER_WG_IPV4
+		read -rp "Server's WireGuard IPv4: " -e -i 10.0.0.1 SERVER_WG_IPV4
 	done
 
 	until [[ ${SERVER_WG_IPV6} =~ ^([a-f0-9]{1,4}:){3,4}: ]]; do
